7a121b2882a126135d994907c12acb52916d4a77
Andrew Iovanna <and.iovanna@gmail.com>
2015-04-12 22:22:25 -0400

    DONE

--- src/com/starboardland/pedometer/CounterActivity.java
+++ src/com/starboardland/pedometer/CounterActivity.java
@@ -6,14 +6,34 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.hardware.*;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
-
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Handler.Callback;
 
-public class CounterActivity extends Activity implements SensorEventListener {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class CounterActivity extends FragmentActivity implements SensorEventListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private SensorManager sensorManager;
     private TextView count1;
@@ -37,7 +57,91 @@ public class CounterActivity extends Activity implements SensorEventListener {
     private float stepsInApp = 0;
     private float stepsInMinute = 0;
 
+    private GoogleApiClient mGoogleApiClient;
+    public static final String TAG = CounterActivity.class.getSimpleName();
+    private LocationRequest mLocationRequest;
+    private GoogleMap mMap;
 
+    long starttime = 0;
+    //this  posts a message to the main thread from our timertask
+    //and updates the textfield
+    final Handler h = new Handler(new Callback() {
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            long millis = System.currentTimeMillis() - starttime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds     = seconds % 60;
+
+            //text.setText(String.format("%d:%02d", minutes, seconds));
+            return false;
+        }
+    });
+    //runs without timer be reposting self
+    Handler h2 = new Handler();
+    Runnable run = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - starttime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds     = seconds % 60;
+
+            //text3.setText(String.format("%d:%02d", minutes, seconds));
+
+            h2.postDelayed(this, 500);
+        }
+    };
+
+    //tells handler to send a message
+    class firstTask extends TimerTask {
+
+        @Override
+        public void run() {
+            h.sendEmptyMessage(0);
+        }
+    };
+
+    //tells activity to run on ui thread
+    class secondTask extends TimerTask {
+
+        @Override
+        public void run() {
+            CounterActivity.this.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    //start_time
+                    long currentTime = System.nanoTime();
+                    long elapsedTimeInSeconds = (currentTime - start_time) / 1000000000;
+                    int seconds = (int) elapsedTimeInSeconds;
+//                    long millis = System.currentTimeMillis() - starttime;
+//                    int seconds = (int) (millis / 1000);
+//                    int minutes = seconds / 60;
+//                    seconds     = seconds % 60;
+                    System.out.println("Seconds: " + seconds);
+                    if(seconds % 60 == 0 && seconds != 0 && seconds <= 480) {
+                        int minute = seconds/ 60;
+                        Toast.makeText(getApplicationContext(), "You took " +  minuteSteps[minute-1] + " steps in segment " + minute + ".", Toast.LENGTH_SHORT).show();
+                        if(seconds == 480) {
+                            //get total
+                            float totalNumber = 0;
+                            for (Float f : minuteSteps) {
+                                totalNumber += f;
+                            }
+                            textViews[8].setText(Float.toString(totalNumber));
+                        }
+                    }
+                    //text2.setText(String.format("%d:%02d", minutes, seconds));
+                }
+            });
+        }
+    };
+
+
+    Timer timer = new Timer();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,10 +164,26 @@ public class CounterActivity extends Activity implements SensorEventListener {
         textViews[7] = count8;
         countTotal = (TextView) findViewById(R.id.count_total);
         textViews[8] = countTotal;
+
+        timer.schedule(new secondTask(),  0,1000);
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         start_time = System.nanoTime();
         StepCounterOpenHelper db = new StepCounterOpenHelper(this);
         database = db.getWritableDatabase();
+
+        setUpMapIfNeeded();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
     }
 
     @Override
@@ -76,6 +196,7 @@ public class CounterActivity extends Activity implements SensorEventListener {
         } else {
             Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
         }
+        mGoogleApiClient.connect();
 
     }
 
@@ -84,7 +205,11 @@ public class CounterActivity extends Activity implements SensorEventListener {
         super.onPause();
         activityRunning = false;
         // if you unregister the last listener, the hardware will stop detecting step events
-//        sensorManager.unregisterListener(this); 
+//        sensorManager.unregisterListener(this);
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
     }
 
     private float calculateStepsSoFar(TextView view, int minNumber, float eventval) {
@@ -200,7 +325,6 @@ public class CounterActivity extends Activity implements SensorEventListener {
                 for (Float f : minuteSteps) {
                     totalNumber += f;
                 }
-                System.out.println("I THINK THIS THE TOTAL: " + totalNumber);
                 textViews[minute].setText(Float.toString(totalNumber));
             }
 
@@ -210,5 +334,58 @@ public class CounterActivity extends Activity implements SensorEventListener {
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+        else {
+            handleNewLocation(location);
+        };
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
+        //CameraUpdateFactory.zoomTo(15);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
     }
 }
